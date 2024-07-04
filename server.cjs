e75289b02345b7a6af4565de17aba2ddd71e52b9
Marcello Henrique <mhmjunior@rd.com.br>
2024-07-03 23:43:37 -0400

    chore: create server file

---
+++ server.cjs
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = 443;
+
+app.use(express.static(path.resolve(__dirname, 'dist')));
+app.get('*', (_, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
